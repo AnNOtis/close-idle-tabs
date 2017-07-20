6383b666dcc5c30ae69df12cabdb7d02734cba3f
Otis Chou <altispec@gmail.com>
2017-07-20 22:43:11 +0800

    attach real data to component

--- src/js/popup/App.js
+++ src/js/popup/App.js
@@ -3,15 +3,46 @@ import Header from './Header'
 import Main from './Main'
 
 class App extends Component {
-  render ({data}) {
-    return <div>
-      <Header wantedTabs={data.wantedTabs} />
-      <Main
-        idleTime={data.IDLE_TIME}
-        unwantedTabs={data.unwantedTabs}
-        wantedTabs={data.wantedTabs}
-      />
-    </div>
+  componentDidMount () {
+    this.intervalfetchData()
+  }
+
+  componentWillUnmount () {
+    this.clearFetchDataTimer()
+  }
+
+  render (_, {data}) {
+    return (
+      !data
+        ? <div>loading...</div>
+        : <div>
+          <Header wantedTabs={data.wantedTabs} />
+          <Main
+            idleTime={data.IDLE_TIME}
+            unwantedTabs={data.unwantedTabs}
+            wantedTabs={data.wantedTabs}
+          />
+        </div>
+    )
+  }
+
+  intervalfetchData () {
+    return this.fetchData()
+      .then(data => {
+        this.setState({ data })
+
+        this._timer = setTimeout(() => {
+          this.intervalfetchData()
+        }, 2000)
+      })
+  }
+
+  clearFetchDataTimer () {
+    clearInterval(this._timer)
+  }
+
+  fetchData () {
+    return chrome.extension.getBackgroundPage().getPopupPageData()
   }
 }
 
