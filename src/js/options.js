6ca515bc142de4d2611e3299de7f3f029effe3c8
Otis Chou <altispec@gmail.com>
2017-07-22 21:41:09 +0800

    add options page

--- src/js/options.js
+++ src/js/options.js
@@ -1,1 +1,24 @@
-import '../css/options.css'
+function saveOptions () {
+  const idleTime = parseInt(document.getElementById('idle-time').value)
+  chrome.storage.sync.set({
+    idleTime
+  }, function () {
+    const status = document.getElementById('hint')
+    status.textContent = 'Options saved.'
+    setTimeout(function () {
+      status.textContent = ''
+    }, 750)
+  })
+}
+
+function restoreOptions () {
+  chrome.storage.sync.get({
+    idleTime: 5000
+  }, function (items) {
+    document.getElementById('idle-time').value = items.idleTime
+  })
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions)
+document.getElementById('save').addEventListener('click', saveOptions)
+document.getElementById('cancel').addEventListener('click', () => window.close())
--- src/js/popup.js
+++ src/js/popup.js
@@ -4,3 +4,13 @@ import '../css/popup.sass'
 import App from './popup/App'
 
 render(<App />, document.getElementById('container'))
+
+document.getElementById('link-to-options').addEventListener('click', function (e) {
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage()
+  } else {
+    window.open(chrome.runtime.getURL('options.html'))
+  }
+
+  e.preventDefault()
+})
